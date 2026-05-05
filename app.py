2fc2e0b3c6b714a095224b007c0b07f214abe0f9
moohmmedmoody310 <moohmmedmoody310@gmail.com>
2026-05-05 11:23:59 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -834,4 +834,6 @@ if __name__ == '__main__':
     print("  المستخدم: admin")
     print("  كلمة المرور: admin123")
     print("="*50 + "\n")
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
